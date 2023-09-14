caafcabe1994e316046cb5fdac54a0bba82147e7
Dylan D <dore.dylan31@gmail.com>
2023-09-15 00:55:17 +0200

    fix texture + start fullscreen event

--- main.js
+++ main.js
@@ -28,17 +28,20 @@ export default class Sktech {
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.container.appendChild(this.renderer.domElement);
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.materials = [];
 
-    this.asscroll = new ASScroll();
+    this.asscroll = new ASScroll({
+      disableRaf: true,
+    });
 
     this.asscroll.enable({
       horizontalScroll: true,
     });
 
     this.time = 0;
-    this.setupSettings();
-    this.resize();
+    // this.setupSettings();
     this.addObjects();
+    this.resize();
     this.render();
     this.setupResize();
   }
@@ -57,6 +60,26 @@ export default class Sktech {
     this.renderer.setSize(this.width, this.height);
     this.camera.aspect = this.width / this.height;
     this.camera.updateProjectionMatrix();
+
+    this.materials.forEach((m) => {
+      m.uniforms.uResolution.value.x = this.width;
+      m.uniforms.uResolution.value.y = this.height;
+    });
+
+    this.imageStore.forEach((item) => {
+      let bounds = item.img.getBoundingClientRect();
+      item.mesh.scale.set(bounds.width, bounds.height, 1);
+      item.top = bounds.top;
+      item.left = bounds.left + this.asscroll.currentPos;
+      item.width = bounds.width;
+      item.height = bounds.height;
+
+      item.mesh.material.uniforms.uQuadSize.value.x = bounds.width;
+      item.mesh.material.uniforms.uQuadSize.value.y = bounds.height;
+
+      item.mesh.material.uniforms.uTextureSize.value.x = bounds.width;
+      item.mesh.material.uniforms.uTextureSize.value.y = bounds.height;
+    });
   }
 
   setupResize() {
@@ -80,52 +103,39 @@ export default class Sktech {
       fragmentShader: fragment,
     });
 
-    this.tl = gsap
-      .timeline()
-      .to(this.material.uniforms.uCorners.value, {
-        x: 1,
-        duration: 1,
-      })
-      .to(
-        this.material.uniforms.uCorners.value,
-        {
-          y: 1,
-          duration: 1,
-        },
-        0.1
-      )
-      .to(
-        this.material.uniforms.uCorners.value,
-        {
-          z: 1,
-          duration: 1,
-        },
-        0.2
-      )
-      .to(
-        this.material.uniforms.uCorners.value,
-        {
-          w: 1,
-          duration: 1,
-        },
-        0.3
-      );
-
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     this.mesh.scale.set(300, 300, 1);
     // this.scene.add(this.mesh);
     this.mesh.position.x = 300;
 
     this.images = [...document.querySelectorAll(".js-image")];
-    this.materials = [];
     this.imageStore = this.images.map((img) => {
       let bounds = img.getBoundingClientRect();
       let m = this.material.clone();
       this.materials.push(m);
-      let texture = new THREE.Texture(img);
-      texture.needsUpdate = true;
+      let texture = new THREE.TextureLoader().load(img.src);
 
       m.uniforms.uTexture.value = texture;
+
+               img.addEventListener('mouseout',()=>{
+                this.tl = gsap.timeline()
+                .to(m.uniforms.uCorners.value,{
+                    x:0,
+                    duration: 0.4
+                })
+                .to(m.uniforms.uCorners.value,{
+                    y:0,
+                    duration: 0.4
+                },0.1)
+                .to(m.uniforms.uCorners.value,{
+                    z:0,
+                    duration: 0.4
+                },0.2)
+                .to(m.uniforms.uCorners.value,{
+                    w:0,
+                    duration: 0.4
+                },0.3)
+            })
 
       let mesh = new THREE.Mesh(this.geometry, m);
       this.scene.add(mesh);
@@ -143,16 +153,18 @@ export default class Sktech {
 
   setPosition() {
     this.imageStore.forEach((o) => {
-      o.mesh.position.x = -this.asscroll.currentPos + o.left - this.width/2 + o.width/2;
-      o.mesh.position.y = -o.top + this.height/2 - o.height/2;
+      o.mesh.position.x =
+        -this.asscroll.currentPos + o.left - this.width / 2 + o.width / 2;
+      o.mesh.position.y = -o.top + this.height / 2 - o.height / 2;
     });
   }
 
   render() {
     this.time += 0.05;
     this.material.uniforms.time.value = this.time;
-    this.material.uniforms.uProgress.value = this.settings.progress;
+    // this.material.uniforms.uProgress.value = this.settings.progress;
     // this.tl.progress(this.settings.progress);
+    this.asscroll.update();
     this.setPosition();
     this.mesh.rotation.x = this.time / 2000;
     this.mesh.rotation.y = this.time / 1000;
